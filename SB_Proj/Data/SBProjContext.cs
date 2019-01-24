b88c696e77d9997849a650ff519405449c9c5f5d
Dydzio <blood990@gmail.com>
2019-01-25 00:27:43 +0100

    Generate API controller, set up program data context

---
+++ SB_Proj/Data/SBProjContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SB_Proj.Models
+{
+    public class SBProjContext : DbContext
+    {
+        public SBProjContext (DbContextOptions<SBProjContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SB_Proj.Models.Book> Book { get; set; }
+    }
+}
