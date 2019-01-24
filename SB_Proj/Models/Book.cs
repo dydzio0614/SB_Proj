cbfed6c4210891c91e602bf24e3193aa554fbc95
Dydzio <blood990@gmail.com>
2019-01-25 00:15:19 +0100

    Initial draft of model data

---
+++ SB_Proj/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SB_Proj.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Amount { get; set; }
+    }
+}
