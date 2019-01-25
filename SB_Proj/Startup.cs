a1205eea945c963ac76bfdce97ebe1521e85240d
Dydzio <blood990@gmail.com>
2019-01-25 22:13:31 +0100

    Set permissive CORS policy

--- SB_Proj/Startup.cs
+++ SB_Proj/Startup.cs
@@ -27,6 +27,19 @@ namespace SB_Proj
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SBProjContext>(options =>
@@ -46,6 +59,7 @@ namespace SB_Proj
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
             app.UseMvc();
         }
     }
